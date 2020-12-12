5fa340472e07f00f8f5b0c2a7c351be4bcfd111a
Ramon de Vaan <ramon.de.vaan@outlook.com>
2020-12-12 23:27:35 +0100

    Prepare day 13

---
+++ app/src/test/java/nl/ramondevaan/aoc2020/day13/Day13Test.java
@@ -0,0 +1,34 @@
+package nl.ramondevaan.aoc2020.day13;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day13Test {
+    Day13 day13;
+
+    @BeforeEach
+    void setUp() throws URISyntaxException, IOException {
+        Path path = Path.of(getClass().getResource("/input/day_13.txt").toURI());
+        List<String> lines = Files.readAllLines(path);
+        day13 = new Day13(lines);
+    }
+
+    @Test
+    void puzzle1() {
+        assertEquals(1956L, day13.solve1());
+    }
+
+    @Test
+    void puzzle2() {
+        assertEquals(136797L, day13.solve2());
+    }
+
+}
