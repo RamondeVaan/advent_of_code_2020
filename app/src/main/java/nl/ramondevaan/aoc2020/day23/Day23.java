a6cf1a954ade25b13679a8c049d7e6d9953590dc
Ramon de Vaan <ramon.de.vaan@outlook.com>
2020-12-22 15:41:58 +0100

    Prepare day 23

---
+++ app/src/main/java/nl/ramondevaan/aoc2020/day23/Day23.java
@@ -0,0 +1,18 @@
+package nl.ramondevaan.aoc2020.day23;
+
+import java.util.List;
+
+public class Day23 {
+
+    public Day23(List<String> lines) {
+
+    }
+
+    public long solve1() {
+        return 0L;
+    }
+
+    public long solve2() {
+        return 0L;
+    }
+}
---
+++ app/src/test/java/nl/ramondevaan/aoc2020/day23/Day23Test.java
@@ -0,0 +1,35 @@
+package nl.ramondevaan.aoc2020.day23;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day23Test {
+
+    static Day23 day23;
+
+    @BeforeAll
+    static void setUp() throws URISyntaxException, IOException {
+        Path path = Path.of(Day23Test.class.getResource("/input/day_23.txt").toURI());
+        List<String> lines = Files.readAllLines(path);
+        day23 = new Day23(lines);
+    }
+
+    @Test
+    void puzzle1() {
+        assertEquals(235L, day23.solve1());
+    }
+
+    @Test
+    void puzzle2() {
+        assertEquals(309L, day23.solve2());
+    }
+
+}
